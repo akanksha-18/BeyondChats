c12c86d39e082883e5609125e82cbcea6b8e8947
Akanksha Dubey <akankshadubey2558@gmail.com>
2025-01-31 00:48:31 +0530

    frontend connected

--- chatbot_backend/index.js
+++ chatbot_backend/index.js
@@ -59,7 +59,7 @@ app.use(cors({
 app.use('/api', (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', [
     'http://localhost:5173',  // Local development URL
-    'https://beyond-chats-jjlr0mklx-akanksha-dubeys-projects.vercel.app'
+    'https://beyond-chats-jjlr0mklx-akanksha-dubeys-projects.vercel.app',
     'https://beyond-chats-sooty.vercel.app'  // Correct Vercel frontend URL
   ]);
   res.setHeader('Access-Control-Allow-Credentials', 'true');
